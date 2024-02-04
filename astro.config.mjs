5a63d7014320821d1fe79884498547c78b21a8d1
Daniil <folco1999@gmail.com>
2024-02-04 14:08:17 +0100

    Second commit. Not everything committed in the first commit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                blogPost: 'storyblok/BlogPost',
+                blogPostList: 'storyblok/BlogPostList',
+                page: 'storyblok/Page',
+            },
+            apiOptions: {
+                region: 'eu',
+            },
+        })
+    ],
+});
