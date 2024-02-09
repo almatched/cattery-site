54476b5e5ca92f52be86f68aa2672916b22ecaf2
almatched <folco1999@gmail.com>
2024-02-09 16:33:31 +0100

    Upgrade: change blog to profiles
    Changed Storyblok integration to show profiles of cats instead of blog posts. Changed page navigation links to be managed by Storyblok.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,20 @@ const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
+    redirects: {
+        '/home': '/',
+    },
     integrations: [
         storyblok({
             accessToken: env.STORYBLOK_TOKEN,
             components: {
-                blogPost: 'storyblok/BlogPost',
-                blogPostList: 'storyblok/BlogPostList',
                 page: 'storyblok/Page',
+                grid: 'storyblok/Grid',
+                feature: 'storyblok/Feature',
+                teaser: 'storyblok/Teaser',
+                config: 'storyblok/Config',
+                profile: 'storyblok/Profile',
+                'all-profiles': 'storyblok/AllProfiles',
             },
             apiOptions: {
                 region: 'eu',
