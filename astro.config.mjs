e0d1d2a4b574374d3e1a0f609af05d79021ce2a2
almatched <folco1999@gmail.com>
2024-02-14 20:36:40 +0100

    Upgrade: add tailwind, add shadcn/ui, add theme toggle from shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,47 +1,52 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl'
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [basicSsl()],
-        server: {
-            https: true,
-        },
-    },
-    redirects: {
-        '/home': '/',
-        '/en/home': '/en/',
-    },
-    integrations: [
-        storyblok({
-            accessToken: env.STORYBLOK_TOKEN,
-            components: {
-                page: 'storyblok/Page',
-                grid: 'storyblok/Grid',
-                feature: 'storyblok/Feature',
-                teaser: 'storyblok/Teaser',
-                config: 'storyblok/Config',
-                profile: 'storyblok/Profile',
-                'all-profiles': 'storyblok/AllProfiles',
-                'about-page': 'storyblok/AboutPage',
-                'contact-page': 'storyblok/Contact',
-                'gallery-page': 'storyblok/GalleryPage',
-                'home-page': 'storyblok/HomePage',
-                'values-list': 'storyblok/ValuesList',
-                entry: 'storyblok/Entry',
-                'menu-link': 'storyblok/MenuLink',
-                'all-information-posts': 'storyblok/AllInformationPosts',
-                'information-post': 'storyblok/InformationPost',
-                parent: 'storyblok/Parent',
-            },
-            apiOptions: {
-                region: 'eu',
-            },
-        })
-    ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true
+    }
+  },
+  redirects: {
+    '/home': '/',
+    '/en/home': '/en/'
+  },
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: 'storyblok/Page',
+        grid: 'storyblok/Grid',
+        feature: 'storyblok/Feature',
+        teaser: 'storyblok/Teaser',
+        config: 'storyblok/Config',
+        profile: 'storyblok/Profile',
+        'all-profiles': 'storyblok/AllProfiles',
+        'about-page': 'storyblok/AboutPage',
+        'contact-page': 'storyblok/Contact',
+        'gallery-page': 'storyblok/GalleryPage',
+        'home-page': 'storyblok/HomePage',
+        'values-list': 'storyblok/ValuesList',
+        entry: 'storyblok/Entry',
+        'menu-link': 'storyblok/MenuLink',
+        'all-information-posts': 'storyblok/AllInformationPosts',
+        'information-post': 'storyblok/InformationPost',
+        parent: 'storyblok/Parent'
+      },
+      apiOptions: {
+        region: 'eu'
+      }
+    }),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    })]
 });
