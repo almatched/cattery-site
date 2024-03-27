99e3ee76f22db9f0898486bc2644511c2e0a4488
Daniil <folco1999@gmail.com>
2024-03-27 20:54:32 +0100

    fix: move class attribute
    Styling was not properly applied. Clickable area was less than what was indicated by padding.

--- src/components/NavigationList.jsx
+++ src/components/NavigationList.jsx
@@ -4,16 +4,16 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu.tsx";
-import { cn } from "@/lib/utils"
 
 export function NavigationList({ data }) {
   return (
     <NavigationMenu className="hidden lg:block">
       <NavigationMenuList className="text-lg flex items-center gap-5">
         {data?.map((menu, key) => (
-          <NavigationMenuItem key={key} className="bg-accent px-3 py-2 rounded-md hover:opacity-75">
+          <NavigationMenuItem key={key}>
             <NavigationMenuLink asChild>
               <a
+                className="bg-accent px-3 py-2 rounded-md hover:opacity-75"
                 href={
                   menu.link.cached_url.startsWith("/")
                     ? menu.link.cached_url
