cb47fafb335fe57cac1fc3ab3a7a8536b59073b3
Daniil <folco1999@gmail.com>
2024-06-06 16:24:08 +0200

    fix: fix padding and cursor pointer to be consistent on language and theme selectors

--- src/components/SelectLanguage.jsx
+++ src/components/SelectLanguage.jsx
@@ -17,7 +17,13 @@ export function SelectLanguage({ data, langSwitch }) {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        {data?.map((lang, key) => <DropdownMenuItem key={key}><a href={langSwitch[lang]} className="w-full">{lang}</a></DropdownMenuItem>)}
+        {data?.map((lang, key) =>
+          <DropdownMenuItem key={key} className="p-0">
+            <a href={langSwitch[lang]} className="w-full px-2 py-1.5">
+              {lang}
+            </a>
+          </DropdownMenuItem>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
